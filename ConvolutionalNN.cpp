2088034131c7abe8c2ac27b7e09fc06ab3a33c31
W1nson <40487548+W1nson@users.noreply.github.com>
2020-06-08 17:47:33 -0700

    Add files via upload

--- ConvolutionalNN.cpp
+++ ConvolutionalNN.cpp
@@ -1,17 +1,56 @@
 #include <iostream> 
+#include <stdlib.h>
 
 using namespace std;
 
 class Matrix 
 {
-    Matrix(int row, int col); 
-    void add(Matrix temp); 
-    void subtract(Matrix temp); 
-    void multiply(Matrix temp); 
-    void modi(string function); 
-    void print()
-    ~Matrix(); 
+    public: 
+        Matrix(int row, int col); 
+        void random(); 
+        void add(Matrix temp); 
+        void subtract(Matrix temp); 
+        void multiply(Matrix temp); 
+        void modi(string function); 
+        void print()
+        ~Matrix(); 
+
+    private: 
+        int row, col; 
+        float ary[][];
 }
+
+Matrix::Matrix(int row, int col)
+{
+    Matrix.row = row; 
+    Matrix.col = col; 
+    Matrix.ary[row][col]; 
+}
+
+Matrix::random()
+{
+    for(int i = 0; i < Matrix.row; i++)
+    {
+        for(int j = 0; j < Matrix.col; j++)
+        {
+            Matrix.ary[i][j] = rand()
+        }
+    }
+    
+}
+
+Matrix::print()
+{
+    for(int i = 0; i < Matrix.row; i++)
+    {
+        for(int j = 0; j < Matrix.col; j++)
+        {
+            std::cout << "Array[" << i << "][" << j << "]: " << Matrix.ary[i][j] << std::endl; 
+        }
+    }
+}
+
+
 
 class Network 
 {
@@ -23,8 +62,16 @@ class Network
 
 class layer
 {
-    layer(string filter, string activa, double size[]); 
+    layer(string filter, string activa, float size[]); 
     void flatten(); 
     void Dense(); 
     void Conv2D();
+}
+
+
+int main()
+{
+    Matrix test = new Matrix::Matrix(1,3); 
+    test.random(); 
+    test.print(); 
 }
